5f67017ee699ae06c3adb36f17bfd3d5667eb218
Prakhar Goel <git@prakhargoel.com>
2022-01-05 23:37:25 +0530

    new changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     // path: path.resolve(__dirname, "react-test/public/dist"), // * react-text
     path: path.resolve(__dirname, "dist"),
-    filename: "maxtap_pubilc.js",
+    filename: "maxtap_public.js",
     library: "Maxtap",
     libraryTarget: "umd",
     umdNamedDefine: true
