032c26c775c500fad983d95546fcc0516e184eec
CHARAN SRINIVAS <charansrinivassajja@gmail.com>
2022-01-06 21:10:06 +0530

    Dynamic 🔗Linking🔗 of css according to javascipt version

--- update_version.sh
+++ update_version.sh
@@ -4,7 +4,5 @@ PACKAGE_VERSION=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
-
 echo "Current npm Version -> $PACKAGE_VERSION"
-
 sed -i "s/%NPM_VERSION%/$PACKAGE_VERSION/g" dist/*.js dist/*.map dist/*.ts
