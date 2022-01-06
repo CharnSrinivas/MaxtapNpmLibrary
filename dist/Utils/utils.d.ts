ac1e5953131c955c4b877ce7cc9e84c5953ab7e9
CHARAN SRINIVAS <charansrinivassajja@gmail.com>
2022-01-06 21:09:37 +0530

    Dynamic 🔗Linking🔗 of css according to javascipt version

---
+++ dist/Utils/utils.d.ts
@@ -0,0 +1,1 @@
+export declare const queryData: (file_name: string) => Promise<[]>;
---
+++ dist/app.d.ts
@@ -0,0 +1,36 @@
+interface PluginData {
+    contend_id: string;
+}
+declare global {
+    interface Window {
+        Maxtap: any;
+        gtag: any;
+        dataLayer: any[];
+    }
+}
+export declare class Component {
+    private video?;
+    private parentElement;
+    private component_start_time;
+    private component_end_time;
+    private product_details?;
+    private image_url?;
+    private redirect_url?;
+    private current_component_index;
+    private component_data?;
+    private interval_id;
+    private is_component_showing;
+    private content_id;
+    private image_loaded;
+    constructor(data: PluginData);
+    init: () => void;
+    private initializeComponent;
+    private prefetchImage;
+    private canComponentDisplay;
+    private canCloseComponent;
+    private removeCurrentComponent;
+    private displayComponent;
+    private onComponentClick;
+    private setRequiredComponentData;
+}
+export {};
